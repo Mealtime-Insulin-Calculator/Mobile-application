20f0eb0a6c1edafc5af889f19d5471ac55c59b7c
Flawnpiece <Zyoulou_@outlook.fr>
2021-04-04 19:05:34 -0400

    small change

--- app/src/main/java/com/example/sqlite/MainActivity.java
+++ app/src/main/java/com/example/sqlite/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        //getSupportActionBar().hide();
+        
 
         setContentView(R.layout.main_activity);
 
